370d4dd80eb7439742aea41bf6a61196a6408f02
Anubhav Gupta <anubhavgu2002@gmail.com>
2024-11-12 17:40:04 +0530

    trafficSource added

--- src/app/(admin)/product-management/page.tsx
+++ src/app/(admin)/product-management/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import toast, { Toaster } from "react-hot-toast";
-import Profile from "../../../assets/images/profile.jpg";
-import { CiSearch } from "react-icons/ci";
-import { FiBell } from "react-icons/fi";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoSettingsOutline } from "react-icons/io5";
 import Table from "../_components/table";
+import TableTraffic from "../_components/tableTrafficSource";
 import Iphone from "../../../assets/images/iphone.png";
 
 const ProductManagement = () => {
+  const [isAllProduct, setIsAllProduct] = useState(true);
   const data = [
     {
       productName: "iPhone 16 Pro",
       category: "Smart Phone",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1475.5K",
@@ -25,7 +21,7 @@ const ProductManagement = () => {
     {
       productName: "MacBook Pro",
       category: "Laptop, Notebook",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1500.5K",
@@ -34,7 +30,7 @@ const ProductManagement = () => {
     {
       productName: "Apple Watch 10",
       category: "Smart Watch",
-      src:Iphone,
+      src: Iphone,
       status: 0,
       price: "$2099",
       pageViews: "200.47K",
@@ -43,7 +39,7 @@ const ProductManagement = () => {
     {
       productName: "Amd Ryzen 9",
       category: "Processor",
-      src:Iphone,
+      src: Iphone,
       status: 2,
       price: "$999.99",
       pageViews: "1475.5K",
@@ -52,7 +48,7 @@ const ProductManagement = () => {
     {
       productName: "iPhone 16 Pro",
       category: "Smart Phone",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1475.5K",
@@ -61,7 +57,7 @@ const ProductManagement = () => {
     {
       productName: "MacBook Pro",
       category: "Laptop, Notebook",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1500.5K",
@@ -70,7 +66,7 @@ const ProductManagement = () => {
     {
       productName: "Apple Watch 10",
       category: "Smart Watch",
-      src:Iphone,
+      src: Iphone,
       status: 0,
       price: "$2099",
       pageViews: "200.47K",
@@ -79,7 +75,7 @@ const ProductManagement = () => {
     {
       productName: "Amd Ryzen 9",
       category: "Processor",
-      src:Iphone,
+      src: Iphone,
       status: 2,
       price: "$999.99",
       pageViews: "1475.5K",
@@ -88,7 +84,7 @@ const ProductManagement = () => {
     {
       productName: "iPhone 16 Pro",
       category: "Smart Phone",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1475.5K",
@@ -97,7 +93,7 @@ const ProductManagement = () => {
     {
       productName: "MacBook Pro",
       category: "Laptop, Notebook",
-      src:Iphone,
+      src: Iphone,
       status: 1,
       price: "$1099",
       pageViews: "1500.5K",
@@ -106,7 +102,7 @@ const ProductManagement = () => {
     {
       productName: "Apple Watch 10",
       category: "Smart Watch",
-      src:Iphone,
+      src: Iphone,
       status: 0,
       price: "$2099",
       pageViews: "200.47K",
@@ -115,10 +111,102 @@ const ProductManagement = () => {
     {
       productName: "Amd Ryzen 9",
       category: "Processor",
-      src:Iphone,
+      src: Iphone,
       status: 2,
       price: "$999.99",
       pageViews: "1475.5K",
+      increment: true,
+    },
+  ];
+  const dataTraffic = [
+    {
+      productName: "iPhone 16 Pro",
+      dayTrading: "01",
+      src: Iphone,
+      weeklyTrading: "05",
+      avgSales: "$9479.99",
+      pageViews: "1475.5K",
+      increment: true,
+    },
+    {
+      productName: "MacBook Pro",
+      dayTrading: "02",
+      src: Iphone,
+      weeklyTrading: "03",
+      avgSales: "$7479.99",
+      pageViews: "1500.5K",
+      increment: true,
+    },
+    {
+      productName: "iPhone 16 Pro",
+      dayTrading: "01",
+      src: Iphone,
+      weeklyTrading: "05",
+      avgSales: "$9479.99",
+      pageViews: "1475.5K",
+      increment: true,
+    },
+    {
+      productName: "MacBook Pro",
+      dayTrading: "02",
+      src: Iphone,
+      weeklyTrading: "03",
+      avgSales: "$7479.99",
+      pageViews: "1500.5K",
+      increment: true,
+    },
+    {
+      productName: "iPhone 16 Pro",
+      dayTrading: "01",
+      src: Iphone,
+      weeklyTrading: "05",
+      avgSales: "$9479.99",
+      pageViews: "1475.5K",
+      increment: true,
+    },
+    {
+      productName: "MacBook Pro",
+      dayTrading: "02",
+      src: Iphone,
+      weeklyTrading: "03",
+      avgSales: "$7479.99",
+      pageViews: "1500.5K",
+      increment: true,
+    },
+    {
+      productName: "iPhone 16 Pro",
+      dayTrading: "01",
+      src: Iphone,
+      weeklyTrading: "05",
+      avgSales: "$9479.99",
+      pageViews: "1475.5K",
+      increment: true,
+    },
+    {
+      productName: "MacBook Pro",
+      dayTrading: "02",
+      src: Iphone,
+      weeklyTrading: "03",
+      avgSales: "$7479.99",
+      pageViews: "1500.5K",
+      increment: true,
+    },
+    {
+      productName: "iPhone 16 Pro",
+      dayTrading: "01",
+      src: Iphone,
+      weeklyTrading: "05",
+      avgSales: "$9479.99",
+      pageViews: "1475.5K",
+      increment: true,
+    },
+    {
+      productName: "MacBook Pro",
+      dayTrading: "02",
+      src: Iphone,
+      weeklyTrading: "03",
+      avgSales: "$7479.99",
+      pageViews: "1500.5K",
       increment: true,
     },
   ];
@@ -128,15 +216,32 @@ const ProductManagement = () => {
 
       <div className="w-full p-4 md:py-4 md:px-8">
         <div className="flex items-center gap-2 mt-5">
-          <div className="px-2.5 py-1.5 bg-gray-800 text-white rounded-full cursor-pointer text-sm">
+          <div
+            className={`px-2.5 py-1.5 rounded-full cursor-pointer text-sm ${
+              isAllProduct
+                ? "bg-gray-800 text-white"
+                : "border border-gray-400 text-gray-400"
+            }`}
+            onClick={() => setIsAllProduct(true)}
+          >
             All Product
           </div>
-          <div className="px-2.5 py-1.5 border border-gray-400 text-gray-400 rounded-full cursor-pointer text-sm">
+          <div
+            className={`px-2.5 py-1.5 rounded-full cursor-pointer text-sm ${
+              !isAllProduct
+                ? "bg-gray-800 text-white"
+                : "border border-gray-400 text-gray-400"
+            }`}
+            onClick={() => setIsAllProduct(false)}
+          >
             Traffic source
           </div>
         </div>
-
-        <Table data={data} />
+        {isAllProduct ? (
+          <Table data={data} />
+        ) : (
+          <TableTraffic dataTraffic={dataTraffic} />
+        )}
       </div>
     </section>
   );
