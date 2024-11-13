9e0886292876d0f3f1f87aeda6396c818d945285
Joel Crasta <loejstarc@gmail.com>
2024-11-13 20:46:18 +0530

    added validation for request project form

--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -5,8 +5,14 @@ import {
   filesSchema,
   preAddIconSchema,
   preAddPackSchema,
+  requestProjectSchema,
 } from "@/utils/zodSchema"
-import type { IIconFormData, IPackFormData, IPackFiles } from "@/utils/types"
+import type {
+  IIconFormData,
+  IPackFormData,
+  IPackFiles,
+  IRequestProjectFormData,
+} from "@/utils/types"
 
 export const validateIconForm = (data: IIconFormData) => {
   try {
@@ -54,6 +60,16 @@ export const validateFiles = (data: IPackFiles) => {
     return true
   } catch (err) {
     toast.error("Please upload all the files.")
+    return false
+  }
+}
+
+export const validateRequestProject = (data: IRequestProjectFormData) => {
+  try {
+    requestProjectSchema.parse(data)
+    return true
+  } catch (err) {
+    toast.error("Please fill all the fields.")
     return false
   }
 }
