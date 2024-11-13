5bbebe807cbb2e01dc097fbf31bb352ae2b4aff9
Joel Crasta <loejstarc@gmail.com>
2024-11-13 20:47:31 +0530

    updated api endpoint

--- src/app/api/request-project/route.ts
+++ src/app/api/request-project/route.ts
@@ -1,36 +1,50 @@
-import { db } from '@/lib/db';
-import { NextResponse } from 'next/server';
+import { db } from "@/lib/db"
+import { NextResponse, NextRequest } from "next/server"
 
-export async function POST(request: Request) {
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+  console.log("Order body:", body)
+
   try {
-    const data = await request.json();
-    console.log(data)
-    const newOrder = await db.order.create({ data });
-    return NextResponse.json(newOrder);
+    await db.order.create({ data: body })
+
+    return NextResponse.json({
+      success: true,
+      message: "Order created successfully",
+    })
   } catch (error) {
-    console.error('Error creating Order:', error);
-    return NextResponse.json({ error: 'Failed to create order' }, { status: 500 });
+    console.error("Error creating Order:", error)
+    return NextResponse.json({
+      success: false,
+      message: "Error creating Order",
+    })
   }
 }
 
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const productId = searchParams.get('productId');
-    try {
-      if (productId) {
-        // Fetch single product by productId
-        const product = await db.product.findUnique({ where: { productId } });
-        if (!product) {
-          return NextResponse.json({ error: 'Product not found' }, { status: 404 });
-        }
-        return NextResponse.json(product);
-      } else {
-        // Fetch all products
-        const products = await db.product.findMany();
-        return NextResponse.json(products);
+  const { searchParams } = new URL(request.url)
+  const productId = searchParams.get("productId")
+  try {
+    if (productId) {
+      // Fetch single product by productId
+      const product = await db.product.findUnique({ where: { productId } })
+      if (!product) {
+        return NextResponse.json(
+          { error: "Product not found" },
+          { status: 404 }
+        )
       }
-    } catch (error) {
-      console.error('Error fetching product(s):', error);
-      return NextResponse.json({ error: 'Failed to retrieve product(s)' }, { status: 500 });
+      return NextResponse.json(product)
+    } else {
+      // Fetch all products
+      const products = await db.product.findMany()
+      return NextResponse.json(products)
     }
+  } catch (error) {
+    console.error("Error fetching product(s):", error)
+    return NextResponse.json(
+      { error: "Failed to retrieve product(s)" },
+      { status: 500 }
+    )
   }
+}
