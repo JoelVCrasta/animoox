275c4e1d508ddc6373e5ecf3283bc74799666e82
Joel Crasta <loejstarc@gmail.com>
2024-11-17 18:29:05 +0530

    added sharp

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        domains: ['res.cloudinary.com'],
-        domains: ['example.com'],
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["res.cloudinary.com", "daqnioq8w8j1r.cloudfront.net"],
+    domains: ["example.com"],
+  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
